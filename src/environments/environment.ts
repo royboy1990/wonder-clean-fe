1f20efdc50cb65cd20d40d0d49b622b25f0a342e
Roy Levy <levyroy1990@gmail.com>
2026-02-08 13:49:56 +0700

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://wonder-clean-env.eba-7mi3c9ri.eu-central-1.elasticbeanstalk.com',
+  apiUrl: 'https://wonder-clean-backend.onrender.com',
   googleApiKey: 'AIzaSyADjOZ6Teq09QJQh3BmgejMDoEMnH61iEM'
 };
